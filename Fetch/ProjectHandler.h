cb63be4a975af6a2703f0787c8e4c8cb4750ebff
Josh Barrow <jbarrow84@me.com>
2015-03-10 11:03:55 -0500

    Document all the things!!!

--- Fetch/ProjectHandler.h
+++ Fetch/ProjectHandler.h
@@ -38,6 +38,12 @@
  */
 +(NSString *)exportProjectAsPlainText:(Projects *)project toUrl:(NSURL *)url;
 
+/**
+ * Helper method that converts the method type enum to a standard NSString
+ *
+ * @param method The int representation of the enum value
+ * @return NSString representation of the method type enum value.
+ */
 +(NSString *)methodStringForMethodType:(int) method;
 
 @end
